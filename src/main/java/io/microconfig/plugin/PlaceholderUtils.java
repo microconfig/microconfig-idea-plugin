4e95b392be8955e525a35fdacc84f2a8800477db
Evgeni Sladkowski <kapodes@gmail.com>
2019-02-17 19:16:34 +0100

    placeholder utils for extracting placeholders from lines

---
+++ src/main/java/io/microconfig/plugin/PlaceholderUtils.java
@@ -0,0 +1,25 @@
+package io.microconfig.plugin;
+
+import java.util.Optional;
+
+import static java.util.Optional.empty;
+
+public class PlaceholderUtils {
+
+    public static String placeholderSubstring(String line, int offset) {
+        return insideBrackets(line, offset).orElse(line);
+    }
+
+    private static Optional<String> insideBrackets(String line, int offset) {
+        int previousStart = line.substring(0, offset + 1).lastIndexOf("${");
+        int previousEnd = line.substring(0, offset + 1).lastIndexOf('}');
+        if (previousEnd > 0 && previousEnd > previousStart) return empty();
+
+        int nextEnd = line.indexOf('}', offset);
+
+        if (previousStart > 0 && nextEnd > 0) return Optional.of(line.substring(previousStart + 2, nextEnd));
+
+        return empty();
+    }
+
+}
---
+++ src/test/java/io/microconfig/plugin/PlaceholderUtilsTest.java
@@ -0,0 +1,35 @@
+package io.microconfig.plugin;
+
+import org.junit.Test;
+
+import static io.microconfig.plugin.PlaceholderUtils.placeholderSubstring;
+import static org.fest.assertions.Assertions.assertThat;
+
+public class PlaceholderUtilsTest {
+
+    private static final String LINE = "server.port=${ports@eureka.port}";
+    private static final String PLACEHOLDER = "ports@eureka.port";
+
+    @Test
+    public void should_resolve_whole_line_if_not_in_brackets() {
+        assertThat(placeholderSubstring(LINE, 7)).isSameAs(LINE);
+    }
+
+    @Test
+    public void should_resolve_substring_if_in_brackets() {
+        assertThat(placeholderSubstring(LINE, 27)).isEqualTo(PLACEHOLDER);
+    }
+
+    @Test
+    public void should_handle_middle_placeholder() {
+        String line = "${first}${middle}${second}";
+        assertThat(placeholderSubstring(line, 11)).isEqualTo("middle");
+    }
+
+    @Test
+    public void should_handle_middle_text() {
+        String line = "${first}middle${second}";
+        assertThat(placeholderSubstring(line, 11)).isEqualTo(line);
+    }
+
+}
