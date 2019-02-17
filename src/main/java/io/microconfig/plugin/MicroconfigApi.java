86cad4fbc7815fa3f3da765cd056df718db6968e
Alex Matorin <alx.matorin@gmail.com>
2019-02-17 17:03:24 +0300

    refactoring

--- src/main/java/io/microconfig/plugin/MicroconfigApi.java
+++ src/main/java/io/microconfig/plugin/MicroconfigApi.java
@@ -1,4 +1,11 @@
 package io.microconfig.plugin;
 
+import java.io.File;
+
 public interface MicroconfigApi {
+    String resolvePlaceholder(String placeholder);
+
+    File sourceOfPlaceholder(String placeholder);
+
+    File dirForComponent(String componentName);
 }
