b76c34ebf9d963264b9803e9ba81da72c3142535
Alex Matorin <alx.matorin@gmail.com>
2019-03-07 00:38:02 +0300

    refactoring

--- src/main/java/io/microconfig/plugin/actions/common/FilePosition.java
+++ src/main/java/io/microconfig/plugin/actions/common/FilePosition.java
@@ -1,11 +1,34 @@
 package io.microconfig.plugin.actions.common;
 
-import lombok.Data;
+import com.intellij.openapi.editor.LogicalPosition;
+import com.intellij.openapi.fileEditor.FileEditorManager;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.psi.PsiFile;
+import lombok.RequiredArgsConstructor;
 
 import java.io.File;
 
-@Data
+import static io.microconfig.plugin.utils.FileUtil.toPsiFile;
+import static io.microconfig.plugin.utils.FileUtil.toVirtualFile;
+
+@RequiredArgsConstructor
 public class FilePosition {
     private final File file;
     private final int lineNumber;
+
+    public void moveTo(Project project) {
+        VirtualFile virtualFile = toVirtualFile(file);
+        PsiFile psiFile = toPsiFile(project, virtualFile);
+        psiFile.navigate(true);
+
+        moveToLine(project, lineNumber);
+    }
+
+    private void moveToLine(Project project, int line) {
+        FileEditorManager.getInstance(project)
+                .getSelectedTextEditor()
+                .getCaretModel()
+                .moveToLogicalPosition(new LogicalPosition(line, 0));
+    }
 }
--- src/main/java/io/microconfig/plugin/actions/jumps/JumpToPlaceholder.java
+++ src/main/java/io/microconfig/plugin/actions/jumps/JumpToPlaceholder.java
@@ -1,19 +1,11 @@
 package io.microconfig.plugin.actions.jumps;
 
-import com.intellij.openapi.editor.LogicalPosition;
-import com.intellij.openapi.fileEditor.FileEditorManager;
-import com.intellij.openapi.project.Project;
-import com.intellij.openapi.vfs.VirtualFile;
-import com.intellij.psi.PsiFile;
 import io.microconfig.plugin.actions.common.ActionHandler;
 import io.microconfig.plugin.actions.common.FilePosition;
 import io.microconfig.plugin.actions.common.PluginContext;
 import io.microconfig.plugin.actions.placeholders.PlaceholderBorders;
 import io.microconfig.plugin.microconfig.MicroconfigApi;
 import lombok.RequiredArgsConstructor;
-
-import static io.microconfig.plugin.utils.FileUtil.toPsiFile;
-import static io.microconfig.plugin.utils.FileUtil.toVirtualFile;
 
 @RequiredArgsConstructor
 public class JumpToPlaceholder implements ActionHandler {
@@ -26,18 +18,6 @@ public class JumpToPlaceholder implements ActionHandler {
         if (placeholder == null || !api.navigatable(placeholder)) return; //todo maybe print a warning
 
         FilePosition filePosition = api.findPlaceholderSource(placeholder, context.currentFile(), context.projectDir());
-        System.out.println("Resolved file position " + filePosition);
-        VirtualFile virtualFile = toVirtualFile(filePosition.getFile());
-        PsiFile psiFile = toPsiFile(context.getProject(), virtualFile);
-        psiFile.navigate(true);
-
-        moveToLine(context.getProject(), filePosition.getLineNumber());
-    }
-
-    private void moveToLine(Project project, int line) {
-        FileEditorManager.getInstance(project)
-                .getSelectedTextEditor()
-                .getCaretModel()
-                .moveToLogicalPosition(new LogicalPosition(line, 0));
+        filePosition.moveTo(context.getProject());
     }
 }
