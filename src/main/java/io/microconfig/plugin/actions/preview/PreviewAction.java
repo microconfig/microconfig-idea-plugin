7e0a211e4f3a3bf06fdb40816d280707ee05be54
Evgeni Sladkovski <evgeni.sladkovskii@trifork.nl>
2019-03-19 18:54:17 +0100

    title for preview

--- src/main/java/io/microconfig/plugin/actions/preview/PreviewAction.java
+++ src/main/java/io/microconfig/plugin/actions/preview/PreviewAction.java
@@ -6,6 +6,7 @@ import io.microconfig.plugin.actions.common.ActionHandler;
 import io.microconfig.plugin.actions.common.MicroconfigAction;
 import io.microconfig.plugin.actions.common.PluginContext;
 import io.microconfig.plugin.microconfig.MicroconfigApi;
+import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import javax.swing.*;
@@ -13,12 +14,14 @@ import javax.swing.*;
 import static javax.swing.ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED;
 
 public class PreviewAction extends MicroconfigAction {
+
     @Override
     protected ActionHandler chooseHandler(PluginContext context) {
         return (ctx, api) -> new PreviewDialog(ctx, api).show();
     }
 
     private static class PreviewDialog extends DialogWrapper {
+
         private final PluginContext context;
         private final MicroconfigApi api;
         private final JComponent textPane;
@@ -30,12 +33,25 @@ public class PreviewAction extends MicroconfigAction {
             this.api = api;
             this.textPane = textPane();
             init();
+            setTitle(context.currentFile().getName());
         }
 
         @Nullable
         @Override
         protected JComponent createCenterPanel() {
             return textPane;
+        }
+
+        @Nullable
+        @Override
+        protected JComponent createNorthPanel() {
+            return new JLabel("North");
+        }
+
+        @NotNull
+        @Override
+        protected Action[] createActions() {
+            return new Action[]{getOKAction()};
         }
 
         private JComponent textPane() {
@@ -49,5 +65,7 @@ public class PreviewAction extends MicroconfigAction {
             scrollPane.setVerticalScrollBarPolicy(VERTICAL_SCROLLBAR_AS_NEEDED);
             return scrollPane;
         }
+
     }
+
 }
