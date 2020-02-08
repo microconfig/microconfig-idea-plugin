ac82ec2342bde40438337fb7a5e4aaefbecfd7dd
Evgeni Sladkowski <kapodes@gmail.com>
2020-02-08 20:54:32 +0100

    release 0.8.2

--- src/main/java/io/microconfig/plugin/run/RunConfigType.java
+++ src/main/java/io/microconfig/plugin/run/RunConfigType.java
@@ -1,11 +1,11 @@
 package io.microconfig.plugin.run;
 
 import com.intellij.execution.configurations.ConfigurationTypeBase;
-import com.intellij.icons.AllIcons;
+import com.intellij.openapi.util.IconLoader;
 
 public class RunConfigType extends ConfigurationTypeBase {
     public RunConfigType() {
-        super("RunConfigType", "Microconfig", "Generate Config", AllIcons.General.Gear);
+        super("RunConfigType", "Microconfig", "Generate Config", IconLoader.getIcon("/logo.png"));
         addFactory(new RunConfigFactory(this));
     }
 }
